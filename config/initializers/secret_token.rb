3449d808dd0f39ca37c273c94d1c77bd1cb6d21c
Tim LaTorre <timlatorre@gmail.com>
2013-01-08 12:27:32 -0500

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SubwayEntrances::Application.config.secret_token = 'e761c574f1ffc8e3dc7125018d618fd3f0fe04d7c4e9c49dc64f0535c104769b10d75b9fa56a65183718b079eb5f52b20c51a0e1e200671c1b960d7b48bc2a0d'
