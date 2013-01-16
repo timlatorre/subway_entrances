a14b3e8b5311633b0605c3657e425ff08ae64a03
Tim LaTorre <timlatorre@gmail.com>
2013-01-16 14:54:00 -0500

    final css and funtionality

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,6 +4,17 @@ class HomeController < ApplicationController
   end
 
   def create
+    html = HTTParty.get('http://data.cityofnewyork.us/api/views/drex-xx56/rows.json')
+    json = JSON(html.body)
+
+    json['data'].each do |entrance|
+      lat = entrance[9][1].to_f
+      long = entrance[9][2].to_f
+      line = entrance[10]
+      name = entrance[12]
+      Subway.create(:lat => lat, :long => long, :line => line, :name => name)
+    end
+    redirect_to root_path
   end
 
   def show
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 SubwayEntrances::Application.routes.draw do
 
-  get '/' => 'home#index'
-  post '/populate' => 'home#create'
+  root :to => 'home#index'
+  get '/populate' => 'home#new'
+  post '/populate' => 'home#create', :as => 'populate_post'
   get '/entrances' => 'home#show'
   post '/search' => 'home#search'
 
